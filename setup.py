9c8263acec13169dacd23f764bb2f3113834c8e5
OpenBagTwo <openbagtwo@gmail.com>
2020-08-29 11:32:48 -0400

    add cruft (from another project)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import versioneer
+
+setup(name='ataglances',
+      description='Custom dashboard for monitoring multiple machines',
+      author='Gili "OpenBagTwo" Barlev',
+      url='https://github.com/OpenBagTwo/ataglances',
+      packages=['ataglances'],
+      license='GPL v3',
+      install_requires=[
+      ],
+      include_package_data=True,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass())
